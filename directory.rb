574ce73e35ee6928ffeb48f90ddb1a8f61c52e8b
samuel-adusei <samuel-adusei@hotmail.com>
2016-12-07 19:39:31 +0000

    Add a list of students

---
+++ directory.rb
@@ -0,0 +1,13 @@
+puts "The students of Villains Academy"
+puts "--------------"
+puts "Dr . Hannibal Lecter"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex DeLarge"
+puts "The wicked Witch of the west"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffrey Baratheon"
+puts "Norman Bates"
+
