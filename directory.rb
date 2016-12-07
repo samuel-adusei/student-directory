0289ac85653adb81cfab160f327973787d2a0579
samuel-adusei <samuel-adusei@hotmail.com>
2016-12-07 21:29:03 +0000

    Added itereation and subarrays

--- directory.rb
+++ directory.rb
@@ -1,14 +1,35 @@
 student_count = 11
-puts "The students of Villains Academy"
-puts "--------------"
-puts "Dr . Hannibal Lecter"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The wicked Witch of the west"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-print "Overall we have #{student_count}"The best students"
+#creates a variable equal 11
+
+students = [
+ "Dr . Hannibal Lecter",
+ "Nurse Ratched",
+ "Michael Corleone",
+ "Alex DeLarge",
+ "The wicked Witch of the west",
+ "Terminator",
+ "Freddy Krueger",
+ "The Joker",
+ "Joffrey Baratheon",
+ "Norman Bates"
+ ]
+ puts "The students of Villians Academy"
+ 
+ puts "-----------"
+ puts students[0]
+ puts students[1]
+ puts students[2]
+ puts students[3]
+ puts students[4]
+ puts students[5]
+ puts students[6]
+ puts students[7]
+ puts students[8]
+ puts students[9]
+ puts students[10]
+print "Overall we have #{student_count}The best students"
+
+students.each do |student|
+ puts student
+end
+
